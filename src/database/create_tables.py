90d0130e4d0e0e28dfeaee664b02d850b669ca29
ehghks021203 <ehghks021203@gmail.com>
2025-02-07 02:25:06 +0900

    feat: mysql database initialize

---
+++ src/database/create_tables.py
@@ -0,0 +1,198 @@
+import os
+from mysql.connector import Error
+from database import create_connection
+
+def create_user(connection: object):
+    try:
+        cursor = connection.cursor()
+        query = '''
+        CREATE TABLE IF NOT EXISTS user (
+            user_id INT AUTO_INCREMENT PRIMARY KEY,
+            role INT NOT NULL DEFAULT 1,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            name VARCHAR(20) NOT NULL,
+            nickname VARCHAR(20) NOT NULL,
+            phone VARCHAR(20),
+            profile_image_url VARCHAR(20),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        '''
+        cursor.execute(query)
+        connection.commit()
+        print('User table created successfully.')
+    except Error as err:
+        print(f'Error: "{err}"')
+    finally:
+        if connection.is_connected():
+            cursor.close()
+
+def create_land_info(connection: object):
+    try:
+        cursor = connection.cursor()
+        query = '''
+        CREATE TABLE IF NOT EXISTS land_info (
+            pnu VARCHAR(20) NOT NULL PRIMARY KEY,
+            land_feature_stdr_year INT NOT NULL,
+            official_land_price FLOAT NOT NULL,
+            predict_land_price FLOAT,
+            land_classification VARCHAR(10) NOT NULL,
+            land_zoning VARCHAR(20) NOT NULL,
+            land_use_situation VARCHAR(20) NOT NULL,
+            land_register VARCHAR(10) NOT NULL,
+            land_area FLOAT NOT NULL,
+            land_height VARCHAR(10) NOT NULL,
+            land_form VARCHAR(10) NOT NULL,
+            road_side VARCHAR(10) NOT NULL,
+            land_uses TEXT,
+            generated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            predicted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        '''
+        cursor.execute(query)
+        connection.commit()
+        print('Land info table created successfully.')
+    except Error as err:
+        print(f'Error: "{err}"')
+    finally:
+        if connection.is_connected():
+            cursor.close()
+
+def create_land_report(connection: object):
+    try:
+        cursor = connection.cursor()
+        query = '''
+        CREATE TABLE IF NOT EXISTS land_report (
+            report_id INT AUTO_INCREMENT PRIMARY KEY,
+            pnu VARCHAR(20) NOT NULL,
+            report TEXT NOT NULL,
+            `like` INT NOT NULL DEFAULT 0,
+            `dislike` INT NOT NULL DEFAULT 0,
+            generated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (pnu) REFERENCES land_info(pnu)
+        );
+        '''
+        cursor.execute(query)
+        connection.commit()
+        print('Land report table created successfully.')
+    except Error as err:
+        print(f'Error: "{err}"')
+    finally:
+        if connection.is_connected():
+            cursor.close()
+
+def create_sale(connection: object):
+    try:
+        cursor = connection.cursor()
+        query = '''
+        CREATE TABLE IF NOT EXISTS sale (
+            sale_id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id INT NOT NULL,
+            pnu VARCHAR(20) NOT NULL,
+            lat DECIMAL(17,14) NOT NULL,
+            lng DECIMAL(17,14) NOT NULL,
+            area FLOAT NOT NULL,
+            price FLOAT NOT NULL,
+            summary TEXT NOT NULL,
+            registered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES user(user_id),
+            FOREIGN KEY (pnu) REFERENCES land_info(pnu)
+        );
+        '''
+        cursor.execute(query)
+        connection.commit()
+        print('Sale table created successfully.')
+    except Error as err:
+        print(f'Error: "{err}"')
+    finally:
+        if connection.is_connected():
+            cursor.close()
+
+def create_trade_history(connection: object):
+    try:
+        cursor = connection.cursor()
+        query = '''
+        CREATE TABLE IF NOT EXISTS trade_history (
+            trade_id INT AUTO_INCREMENT PRIMARY KEY,
+            pnu VARCHAR(20) NOT NULL,
+            land_classification VARCHAR(10) NOT NULL,
+            land_zoning VARCHAR(20) NOT NULL,
+            deal_year INT NOT NULL,
+            deal_month INT NOT NULL,
+            deal_price FLOAT NOT NULL,
+            deal_area FLOAT NOT NULL,
+            deal_type VARCHAR(10)
+        );
+        '''
+        cursor.execute(query)
+        connection.commit()
+        print('Trade history table created successfully.')
+    except Error as err:
+        print(f'Error: "{err}"')
+    finally:
+        if connection.is_connected():
+            cursor.close()
+
+def create_region_coordinate(connection: object):
+    try:
+        cursor = connection.cursor()
+        query = '''
+        CREATE TABLE IF NOT EXISTS region_coordinate (
+            pnu VARCHAR(10) NOT NULL PRIMARY KEY,
+            type VARCHAR(12) NOT NULL,
+            region VARCHAR(50) NOT NULL,
+            lat DECIMAL(17,14) NOT NULL,
+            lng DECIMAL(17,14) NOT NULL
+        );
+        '''
+        cursor.execute(query)
+        connection.commit()
+        print('Region coordinate table created successfully.')
+    except Error as err:
+        print(f'Error: "{err}"')
+    finally:
+        if connection.is_connected():
+            cursor.close()
+
+def create_geometry_data(connection: object):
+    try:
+        cursor = connection.cursor()
+        query = '''
+        CREATE TABLE IF NOT EXISTS geometry_data (
+            pnu VARCHAR(10) NOT NULL PRIMARY KEY,
+            centroid_lat DECIMAL(17,14) NOT NULL,
+            centroid_lng DECIMAL(17,14) NOT NULL,
+            multi_polygon LONGTEXT NOT NULL
+        );
+        '''
+        cursor.execute(query)
+        connection.commit()
+        print('Geometry data table created successfully.')
+    except Error as err:
+        print(f'Error: "{err}"')
+    finally:
+        if connection.is_connected():
+            cursor.close()
+
+def create_user_favorite_land(connection: object):
+    try:
+        cursor = connection.cursor()
+        query = '''
+        CREATE TABLE IF NOT EXISTS user_favorite_land (
+            like_id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id INT NOT NULL,
+            pnu VARCHAR(20) NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES user(user_id),
+            FOREIGN KEY (pnu) REFERENCES land_info(pnu)
+        );
+        '''
+        cursor.execute(query)
+        connection.commit()
+        print('Geometry data table created successfully.')
+    except Error as err:
+        print(f'Error: "{err}"')
+    finally:
+        if connection.is_connected():
+            cursor.close()
---
+++ src/database/make_database.py
@@ -0,0 +1,38 @@
+from mysql.connector import Error
+from database import create_connection, ROOT_PW, USER_NAME, USER_PW, DATABASE_NAME
+
+def create_database(connection: object, query: str) -> None:
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print('# Database created successfully.')
+    except Error as err:
+        print(f'Error: "{err}"')
+
+def create_user(connection: object, username: str, password: str) -> None:
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f'CREATE USER \'{username}\'@\'localhost\' IDENTIFIED BY \'{password}\';')
+        print('# User created successfully.')
+    except Error as err:
+        print(f'Error: "{err}"')
+
+def grant_privileges(connection: object, username: str, database: str) -> None:
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f'GRANT ALL PRIVILEGES ON {database}.* TO \'{username}\'@\'localhost\';')
+        connection.commit()
+        print('# Privileges granted successfully.')
+    except Error as err:
+        print(f'Error: "{err}"')
+
+def make() -> None:
+    print('localhost', 'root', ROOT_PW)
+    connection = create_connection('localhost', 'root', ROOT_PW)
+    create_database(connection, f'CREATE DATABASE {DATABASE_NAME}')
+    create_user(connection, USER_NAME, USER_PW)
+    grant_privileges(connection, USER_NAME, DATABASE_NAME)
+
+    if connection:
+        connection.close()
+        print("# MySQL connection is closed.")
